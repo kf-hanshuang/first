0f673fde90ca98867d1945c622d9d82aba49a5d0
kf-hanshuang <1096419056@qq.com>
2021-06-10 13:05:29 +0800

    helloworld commit

---
+++ helloworld/src/main/java/com/han/maven/Hello.java
@@ -0,0 +1,11 @@
+package com.han.maven;
+
+/**
+ * @author HanSh   Email:1096419056@qq.com
+ * @create 2021-06-10  12:45
+ */
+public class Hello {
+    public void main() {
+        System.out.println("Hello World!");
+    }
+}
---
+++ helloworld/src/test/java/com/han/maven/HelloTest.java
@@ -0,0 +1,25 @@
+package com.han.maven;
+
+import org.junit.Test;
+
+
+/**
+ * Hello Tester.
+ *
+ * @author HanShuang
+ * @since <pre>06/10/2021</pre>
+ */
+public class HelloTest {
+
+    /**
+     * Method: main()
+     */
+    @Test
+    public void MainTest() throws Exception {
+//TODO
+        Hello hello = new Hello();
+        hello.main();
+    }
+
+
+} 
